4c06b923f41caf374bf4ffb2a83591ab341f0132
rjpower <607207+rjpower@users.noreply.github.com>
2025-11-23 03:33:18 +0000

    Update Docker image tag

--- lib/marin/src/marin/cluster/config.py
+++ lib/marin/src/marin/cluster/config.py
@@ -22,7 +22,7 @@ import jinja2
 import yaml
 
 # Cluster configuration constants and templates
-LATEST = "20250923"  # The latest docker tag used for the clusters
+LATEST = "2c81f2c"  # The latest docker tag used for the clusters
 
 
 @dataclass
